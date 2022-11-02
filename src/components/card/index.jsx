ea6edddb42612b7c4fcecb48afc73228c99db72f
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2022-11-02 23:12:17 +0100

    Sending id to url for logement

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,5 +1,6 @@
 import Logements from "../../data.json"
 import "../../style/card/card.scss"
+import { Link } from "react-router-dom"
 
  function Card (){
     return(
@@ -10,11 +11,11 @@ import "../../style/card/card.scss"
             Logements.map( logement =>{
                 return (
                     <div className="coverCard" key={logement.id}>
-                        <a href="/logement/:id" key={logement.id}>
+                        <Link to={`/logement/${logement.id}`} key={logement.id}>
                             <img src={logement.cover} alt="coverPictures" className="coverPictures"/>
                             <p className="cardTitle" key={logement.id}>
                             {logement.title}</p>
-                        </a>
+                        </Link>
                     </div>
                 )
             })
--- src/pages/logement.jsx
+++ src/pages/logement.jsx
@@ -1,19 +1,40 @@
-// Utilisation ici du hook react useparams afin de passer dans l'url l'id du logement concerné 
-import { useParams } from "react-router-dom";
+// Utilisation ici du hook react useparams afin de passer dans l'url l'id du logement concerné
+//import { useParams } from "react-router-dom";
 import React from "react";
- 
-function Logement(){
-    const params = useParams();
 
-    React.useEffect(()=>{
-        console.log("params", params)
-    },[params])
+
+function Logement(){
 
     return(
         <div>
-            <h2>Titre d'un logement</h2>
+            
         </div>
     )
 }
 
 export default Logement;
+
+//TEST 1
+//React.useEffect(()=>{
+    //     console.log("params", params)
+    // },[params])
+
+    //const {id} = useParams();
+    //const {datas} = useFetch("./logements.json")
+    
+//TEST 2
+    //const {id} = useParams;
+    // const [data, setData] = useState([])
+
+    // useEffect(()=>{
+    //     const getData = async () => {
+    //         const result = await fetch('../data.json')
+    //         console.log(result);
+    //     }
+    //     const verifyID = json.find((logement)=> logement.id === id)
+    //     setData(verifyID)
+    //     if(verifyID !== id){
+    //         return <Error/>
+    //     }
+    //     getData()
+    // },[id])
